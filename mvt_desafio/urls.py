19ce2d6248b60c485aaad07bd180fd80bfdf82d3
Tomas Carranza Casares <tomicarranza999@gmail.com>
2022-06-29 06:30:47 -0300

    commit final

--- mvt_desafio/urls.py
+++ mvt_desafio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_desafio.views import lista_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lista-familiares/', lista_familiares),
 ]
