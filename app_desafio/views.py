0a4ccefc1ab070bb90cef7ebb00bbdfc8308179a
Tomas Carranza Casares <tomicarranza999@gmail.com>
2022-06-29 06:50:41 -0300

    ultimo commit del desafio

--- app_desafio/views.py
+++ app_desafio/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from app_desafio.models import Familia
 
-def lista_familiares(request):
+def lista_familia(request):
 
     context = {}
     context['familiares'] = Familia.objects.all() # me traigo todos los objetos, que vienen dentro de una lista
     # los elementos de la lista(en donde estan los objetos) seran los valores de la clave: familiares
 
-    return render(request, 'app_desafio/lista_familiares.html', context)
+    return render(request, 'app_desafio/lista_familia.html', context)
     # y ahora render armara el template con la pagina web(html) y lo trae devuelta para view
     # y view se lo da al servidor y luego el nav me lo va a traer a mi despues de ejecutar runserver
-    
+
--- mvt_desafio/urls.py
+++ mvt_desafio/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_desafio.views import lista_familiares
+from app_desafio.views import lista_familia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lista-familiares/', lista_familiares),
+    path('lista-familiares/', lista_familia),
 ]
